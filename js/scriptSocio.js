9ea5eb19eabd5907c44420f1f301c08a22737c4f
Eduardo Poot <eduardopoot83@gmail.com>
2025-05-26 23:08:37 -0600

    Diseño dashboard socios aun preliminar

--- js/scriptSocio.js
+++ js/scriptSocio.js
@@ -3,3 +3,12 @@ document.querySelectorAll('.barra span').forEach(barra => {
   const valor = barra.style.getPropertyValue('--valor');
   barra.style.width = valor;
 });
+
+//Usuario menu
+
+const toggleBtn = document.getElementById('menuToggle');
+const userPanel = document.querySelector('.user-panel');
+
+toggleBtn.addEventListener('click', () => {
+    userPanel.classList.toggle('open');
+});
